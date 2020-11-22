5baa59f96aeff62fe96379ec683274ed128997c3
axcelcorona <axcelcorona@gmail.com>
2020-11-22 02:52:45 -0500

    Navbar creado

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,9 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="navbar bg-info">
+    <router-link to="/"><h1 class="text-white">Covid-19</h1></router-link> 
+    <router-link to="/lsita"><h4 class="btn btn-info text-white">Listado</h4></router-link>
   </div>
   <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
